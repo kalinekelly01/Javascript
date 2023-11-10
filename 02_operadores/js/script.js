6acf89d72bad7e3021996dd81aebaf78441c013b
Kaline Kelly <145710571+kalinekelly01@users.noreply.github.com>
2023-11-10 11:45:21 -0300

    Aulas de JS

---
+++ 02_operadores/js/script.js
@@ -0,0 +1,17 @@
+//  - Number
+console.log(typeof 2);
+console.log(typeof 5.14);
+console.log(typeof -127);
+
+// 2 - Ops. Aritmeticas
+console.log(2 + 4);
+console.log(10 - 5);
+console.log(2 * 10);
+console.log(20 / 2);
+console.log(9 + 3 * 10);
+
+// 3 - Special numberes
+console.log(typeof Infinity);
+console.log(typeof -Infinity);
+console.log(12 *  "asd");
+console.log(typeof NaN);
