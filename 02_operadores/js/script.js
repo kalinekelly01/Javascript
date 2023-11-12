6c33b315ef5e43d2c5928f8432beb9f6cefa31b0
Kaline Kelly <145710571+kalinekelly01@users.noreply.github.com>
2023-11-12 08:56:27 -0300

    Operadores Js

--- 02_operadores/js/script.js
+++ 02_operadores/js/script.js
@@ -1,4 +1,4 @@
-//  - Number
+// 1 - Number
 console.log(typeof 2);
 console.log(typeof 5.14);
 console.log(typeof -127);
@@ -15,3 +15,65 @@ console.log(typeof Infinity);
 console.log(typeof -Infinity);
 console.log(12 *  "asd");
 console.log(typeof NaN);
+
+// 4 - Textos
+console.log("Um texto");
+console.log('mais um texto');
+console.log(`17`);
+
+console.log(typeof "Um texto");
+console.log(typeof 'mais um texto');
+
+// 5 - Símbolos especiais em String
+console.log("Testando a \n quebra de linha");
+console.log("Espaçamento \t de tab"); 
+
+// 6 - Concatenação
+console.log("Oi," + " tudo " + "bem?");
+console.log(`Testando` + ` com ` + `crase!`);
+
+// 7 - Interpolação
+console.log(`A soma de 2 + 2 é: ${2 + 2}`);
+console.log(`Podemos executar qualquer coisa aqui ${console.log("teste")}`);
+
+// 8 - Boolean
+console.log(true);
+console.log(5 > 20);
+console.log(30 < 10);
+console.log(typeof false);
+
+// 9 - Comparações
+console.log(5 <= 5);
+console.log(5 < 5);
+console.log(10 == 10);
+console.log(10 === 10);
+console.log(10 != 9);
+
+// 10 - Identico
+console.log(9 == "9");
+console.log(9 === "9");
+console.log(9 != "9");
+console.log(9 !== "9");
+
+// 11 - Operadores Lógicos
+console.log(true && true);
+console.log(true && false);
+console.log(5 > 2 && 2 < 10);
+console.log(5 > 2 && "kaline" === 1);
+console.log(5 > 2 || 5 > 100);
+console.log(!true);
+console.log(!5 > 2);
+console.log(!10 === 10);
+
+// 12 - Empty Values
+console.log(typeof null, typeof undefined);
+console.log(null === undefined);
+console.log(null == undefined);
+console.log(null == false);
+console.log(undefined == false);
+
+// 13 - Mudança de tipos
+console.log(5 * null);
+console.log("teste" * "opa");
+console.log("10" + 1);
+console.log("10" - 1);
---
+++ 03_Estruturas/js/script.js
@@ -0,0 +1,51 @@
+// 1 - Variáveis
+let nome = "kaline";
+console.log(nome);
+
+nome = "Kaline Kelly";
+console.log(nome);
+
+//o const não muda seu valor!
+const idade = 21;
+console.log(idade);
+
+console.log(typeof nome);
+console.log(typeof idade);
+
+// 2 - Mais sobre Variáveis
+//let 2test = "inválido"
+//let @test = "inválido"
+
+let a = 10, b = 20, c = 30;
+console.log(a, b, c);
+
+const  nomecompleto = "Kaline Kelly";
+
+const nomeCompleto = "João Vitor";
+
+console.log(nomecompleto);
+
+console.log(nomeCompleto);
+
+let _teste = "ok";
+
+let $teste = "ok";
+
+console.log(_teste, $teste);
+
+// 3 - Prompt
+const age = prompt("Digite sua idade");
+
+console.log(`Voce tem ${age} anos.`);
+
+// 4 - Alert
+alert("Testando");
+
+const z = 10
+
+alert(`O número é ${z}`);
+
+// 5 - Math.x
+console.log(Math.max(5, 2, 3, 19));
+console.log(Math.floor(5.14));
+console.log(Math.ceil(6.29));
